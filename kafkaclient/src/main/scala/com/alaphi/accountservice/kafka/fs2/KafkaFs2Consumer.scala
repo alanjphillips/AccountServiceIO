22f793e8610033df767f27625a2d5c63322b616c
alanp <alanphillips78@gmail.com>
2019-02-08 19:54:56 +0000

    FS2 kafka client Properties WIP

--- kafkaclient/src/main/scala/com/alaphi/accountservice/kafka/fs2/KafkaFs2Consumer.scala
+++ kafkaclient/src/main/scala/com/alaphi/accountservice/kafka/fs2/KafkaFs2Consumer.scala
@@ -46,18 +46,14 @@ object KafkaFs2Consumer {
 object KafkaProperties {
   val default = {
     val props = new Properties()
-    props.putAll(
-      Map(
-        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "kafka-1:9092",
-        ConsumerConfig.GROUP_ID_CONFIG -> "streamgroup",
-        ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> "true",
-        ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG -> "1000",
-        ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG -> "300000",
-        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> "org.apache.kafka.common.serialization.StringDeserializer",
-        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> "org.apache.kafka.common.serialization.StringDeserializer",
-        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "earliest"
-      ).asJava
-    )
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092")
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "streamgroup")
+    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+    props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000")
+    props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "300000")
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
     props
   }
 }
